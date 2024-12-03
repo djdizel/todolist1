435a7529c2781c125beca5d4ff688fa197f5ffd3
djdizel <158520404+djdizel@users.noreply.github.com>
2024-12-03 20:43:52 +0200

    123123

---
+++ todolist/Migrations/20241203183143_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace todolist.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "TaskItems",
+                columns: new[] { "Id", "IsCompleted", "Status", "Title" },
+                values: new object[,]
+                {
+                    { 1, false, "Pending", "Task 1" },
+                    { 2, false, "Completed", "Task 2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "TaskItems");
+        }
+    }
+}
